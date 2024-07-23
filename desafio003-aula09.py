75f897affe6c6ba1e9e29249c383974ad2d685ca
Rafael Gabriel Magalhães <devrafaelmagalhaes@hotmail.com>
2024-07-22 22:22:09 -0300

    Aula 09 + Desafios

---
+++ desafio003-aula09.py
@@ -0,0 +1,2 @@
+cid = str(input('Insira a cidade que você nasceu: ')).strip()
+print(cid[:5].upper() == 'SANTO')
---
+++ desafio004-aula09.py
@@ -0,0 +1,2 @@
+nome = str(input('Qual é seu nome completo? '))
+print('Seu nome tem Silva ? {}'.format('silva' in nome.upper()))
